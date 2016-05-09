fa55362af853d5939e7cc87017e4e1ca0578b48f
tortuvshin <toroo.byamba@gmail.com>
2016-05-10 06:32:44 +0900

    User table adapter үүсгэв

--- app/src/main/java/com/memorize/Database/MyDbHandler.java
+++ app/src/main/java/com/memorize/Database/MyDbHandler.java
@@ -13,7 +13,6 @@ import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
 
 import com.memorize.Model.RememberWord;
-import com.memorize.Model.User;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -33,12 +32,6 @@ public class MyDbHandler extends SQLiteOpenHelper {
     private static final int    DATABASE_VERSION = 1;
     private static final String DATABASE_NAME    = "mydictionary.db";
 
-    public static final String TABLE_USERS   = "users";
-    public static final String USER_ID       = "id";
-    public static final String USER_NAME     = "name";
-    public static final String USER_EMAIL    = "email";
-    public static final String USER_PASSWORD = "password";
-
     public static final String TABLE_WORDS = "words";
     public static final String WORD_ENG    = "english";
     public static final String WORD_TYPE   = "type";
@@ -49,14 +42,14 @@ public class MyDbHandler extends SQLiteOpenHelper {
     public static final String REMEMBER_WORD_TYPE = "remember_word_type";
     public static final String REMEMBER_WORD_MON = "remember_word_mongolia";
 
-    private static final String[] PROJECTIONS_USERS = {USER_ID, USER_NAME, USER_EMAIL,USER_PASSWORD};
+    static final String[] PROJECTIONS_USERS = {
+            UserAdapter.USER_ID,
+            UserAdapter.USER_NAME,
+            UserAdapter.USER_EMAIL,
+            UserAdapter.USER_PASSWORD
+    };
     private static final String[] PROJECTIONS_WORDS = {WORD_ENG, WORD_TYPE, WORD_MON};
     private static final String[] PROJECTIONS_REMEMBER_WORDS = {REMEMBER_WORD_ENG, REMEMBER_WORD_TYPE, REMEMBER_WORD_MON};
-
-    private static final int USER_ID_INDEX       = 0;
-    private static final int USER_NAME_INDEX     = 1;
-    private static final int USER_EMAIL_INDEX    = 2;
-    private static final int USER_PASSWORD_INDEX = 3;
 
     private static final int WORD_ENG_INDEX     = 0;
     private static final int WORD_TYPE_INDEX    = 1;
@@ -67,10 +60,10 @@ public class MyDbHandler extends SQLiteOpenHelper {
     private static final int REMEMBER_WORD_MON_INDEX= 2;
 
     private static final String CREATE_TABLE_USERS = "CREATE TABLE users (" +
-            USER_ID + " INTEGER PRIMARY KEY," +
-            USER_NAME + " TEXT," +
-            USER_EMAIL + " TEXT," +
-            USER_PASSWORD + " TEXT)";
+            UserAdapter.USER_ID + " INTEGER PRIMARY KEY," +
+            UserAdapter.USER_NAME + " TEXT," +
+            UserAdapter.USER_EMAIL + " TEXT," +
+            UserAdapter.USER_PASSWORD + " TEXT)";
 
     private static final String CREATE_TABLE_WORDS = "CREATE TABLE words (" +
             WORD_ENG + " TEXT PRIMARY KEY," +
@@ -136,26 +129,10 @@ public class MyDbHandler extends SQLiteOpenHelper {
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        dropTable(TABLE_USERS);
+        dropTable(UserAdapter.TABLE_USERS);
         dropTable(TABLE_WORDS);
         dropTable(TABLE_REMEMBER_WORDS);
         onCreate(db);
-    }
-    public void addUser(User user) {
-        if (user == null) {
-            return;
-        }
-        SQLiteDatabase db = getWritableDatabase();
-        if (db == null) {
-            return;
-        }
-        ContentValues cv = new ContentValues();
-        cv.put(USER_NAME, user.getUserName());
-        cv.put(USER_EMAIL, user.getUserEmail());
-        cv.put(USER_PASSWORD, user.getUserPassword());
-        // Inserting Row
-        db.insert(TABLE_USERS, null, cv);
-        db.close();
     }
     public void addWord(Word word) {
         if (word == null) {
@@ -187,23 +164,6 @@ public class MyDbHandler extends SQLiteOpenHelper {
         cv.put(REMEMBER_WORD_MON, rememberWord.getRememberMongolia());
         db.insert(TABLE_REMEMBER_WORDS, null, cv);
         db.close();
-    }
-    public User getUser(int id) {
-        SQLiteDatabase db = getReadableDatabase();
-        if (db == null) {
-            return null;
-        }
-        Cursor cursor = db.query(TABLE_USERS, PROJECTIONS_USERS, USER_ID + "=?",
-                new String[]{String.valueOf(id)}, null, null, null, null);
-        if (!cursor.moveToFirst()) {
-            return null;
-        }
-        User user = new User(cursor.getInt(USER_ID_INDEX),
-                cursor.getString(USER_NAME_INDEX),
-                cursor.getString(USER_EMAIL_INDEX),
-                cursor.getString(USER_PASSWORD_INDEX));
-        cursor.close();
-        return user;
     }
     public Word getWord(String id) {
         SQLiteDatabase db = getReadableDatabase();
@@ -238,18 +198,7 @@ public class MyDbHandler extends SQLiteOpenHelper {
         return rememberWord;
     }
 
-    public Cursor checkUser(String username,String password){
 
-        SQLiteDatabase db = getReadableDatabase();
-
-        Cursor cursor = db.query(TABLE_USERS,new String[]{USER_ID,USER_NAME,USER_EMAIL,USER_PASSWORD},
-                USER_NAME +    "='" +username + "' AND " +
-                        USER_PASSWORD + "='"+password+"'",null,null,null,null);
-        if (cursor != null){
-            cursor.moveToFirst();
-        }
-        return cursor;
-    }
     public Cursor checkWord(String english){
 
         SQLiteDatabase db = getReadableDatabase();
@@ -266,30 +215,12 @@ public class MyDbHandler extends SQLiteOpenHelper {
 
         SQLiteDatabase db = getReadableDatabase();
 
-        Cursor cursor = db.query(TABLE_REMEMBER_WORDS,new String[]{REMEMBER_WORD_ENG,REMEMBER_WORD_TYPE,REMEMBER_WORD_MON},
-                REMEMBER_WORD_ENG +    "='" +english +"'",null,null,null,null);
+        Cursor cursor = db.query(TABLE_REMEMBER_WORDS, new String[]{REMEMBER_WORD_ENG, REMEMBER_WORD_TYPE, REMEMBER_WORD_MON},
+                REMEMBER_WORD_ENG + "='" + english + "'", null, null, null, null);
         if (cursor != null){
             cursor.moveToFirst();
         }
         return cursor;
-    }
-    public List<User> getAllUsers() {
-        List<User> users = new ArrayList<>();
-        String selectQuery = "SELECT * FROM " + TABLE_USERS;
-        SQLiteDatabase db = getReadableDatabase();
-        Cursor cursor = db.rawQuery(selectQuery, null);
-        if (cursor.moveToFirst()) {
-            do {
-                int id = cursor.getInt(USER_ID_INDEX);
-                String name = cursor.getString(USER_NAME_INDEX);
-                String email = cursor.getString(USER_EMAIL_INDEX);
-                String password = cursor.getString(USER_PASSWORD_INDEX);
-                User user = new User(id, name, email, password);
-                users.add(user);
-            } while (cursor.moveToNext());
-        }
-        cursor.close();
-        return users;
     }
 
     public List<Word> getAllWords() {
@@ -326,28 +257,11 @@ public class MyDbHandler extends SQLiteOpenHelper {
             } while (cursor.moveToNext());
         }
 
-        Log.d(TAG,""+rwords);
+        Log.d(TAG, "" + rwords);
         cursor.close();
         return rwords;
     }
-    public int updateUser(User user) {
-        if (user == null) {
-            return -1;
-        }
-        SQLiteDatabase db = getWritableDatabase();
-        if (db == null) {
-            return -1;
-        }
-        ContentValues cv = new ContentValues();
-        cv.put(USER_NAME, user.getUserName());
-        cv.put(USER_EMAIL, user.getUserEmail());
-        cv.put(USER_PASSWORD,user.getUserPassword());
-        // Upating the row
-        int rowCount = db.update(TABLE_USERS, cv, USER_ID + "=?",
-                new String[]{String.valueOf(user.getUserId())});
-        db.close();
-        return rowCount;
-    }
+
 
     public int updateWord(Word word) {
         if (word == null) {
@@ -369,18 +283,6 @@ public class MyDbHandler extends SQLiteOpenHelper {
         return rowCount;
     }
 
-    public void deleteUser(User user) {
-        if (user == null) {
-            return;
-        }
-        SQLiteDatabase db = getWritableDatabase();
-        if (db == null) {
-            return;
-        }
-        db.delete(TABLE_USERS, USER_ID + "=?", new String[]{String.valueOf(user.getUserId())});
-        db.close();
-    }
-
     public void deleteWord(Word word) {
         if (word == null) {
             return;
@@ -392,6 +294,7 @@ public class MyDbHandler extends SQLiteOpenHelper {
         db.delete(TABLE_WORDS, WORD_ENG + "=?", new String[]{String.valueOf(word.getEnglish())});
         db.close();
     }
+
     public void deleteRememberWord(RememberWord rememberWord) {
         if (rememberWord == null) {
             return;
@@ -403,19 +306,11 @@ public class MyDbHandler extends SQLiteOpenHelper {
         db.delete(TABLE_REMEMBER_WORDS, WORD_ENG + "=?", new String[]{String.valueOf(rememberWord.getRememberEnglish())});
         db.close();
     }
-    public int getUserCount() {
-        String query = "SELECT * FROM  " + TABLE_USERS;
-        SQLiteDatabase db = getReadableDatabase();
-        Cursor cursor = db.rawQuery(query, null);
-        int count = cursor.getCount();
-        cursor.close();
-        return count;
-    }
     public void dropTable(String tableName) {
         SQLiteDatabase db = getWritableDatabase();
         if (db == null || TextUtils.isEmpty(tableName)) {
             return;
         }
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+        db.execSQL("DROP TABLE IF EXISTS ");
     }
 }
