2491c246e4866c5d3302da7ba4247fb6c6bd5334
tortuvshin <toroo.byamba@gmail.com>
2016-05-10 09:00:24 +0900

    Хайлтын хэсэг fragment дээр байгааг өөрчлөх #25

--- app/src/main/java/com/memorize/MainActivity.java
+++ app/src/main/java/com/memorize/MainActivity.java
@@ -2,32 +2,58 @@ package com.memorize;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import com.memorize.Database.DatabaseHelper;
 import com.memorize.Database.RememberWordsAdapter;
+import com.memorize.Database.WordsAdapter;
 import com.memorize.Model.RememberWord;
+import com.memorize.Model.Word;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, ShakeEventManager.ShakeListener {
     private static final String TAG = "MainActivity";
+
+    public static final String PREFER_NAME = "SearchedWord";
+    private SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    ListView wordList;
+    SearchView searchView;
+    WordsAdapter wordsAdapter;
     RememberWordsAdapter rememberWordsAdapter;
     private ShakeEventManager shakeEventManager;
+    NavigationView navigationView;
+    final ArrayList<String> wordListItems = new ArrayList<String>();
+    ArrayAdapter<String> myArrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +61,7 @@ public class MainActivity extends AppCompatActivity
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        init();
         rememberWordsAdapter = new RememberWordsAdapter(getApplicationContext());
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -44,23 +70,85 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         shakeEventManager = new ShakeEventManager();
         shakeEventManager.setListener(this);
         shakeEventManager.init(this);
-
-        Fragment MainFragment = new MainFragment();
-        FragmentManager fm = getFragmentManager();
-        FragmentTransaction fragmentTransaction= fm.beginTransaction();
-        fragmentTransaction.replace(R.id.container, MainFragment);
-        fragmentTransaction.commit();
     }
+
+    private void init(){
+
+        wordList = (ListView)findViewById(R.id.wordListView);
+
+        wordsAdapter = new WordsAdapter(this);
+        sharedPreferences = getApplicationContext().getSharedPreferences(PREFER_NAME, 0);
+        editor = sharedPreferences.edit();
+
+        myArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,wordListItems);
+        wordList.setAdapter(myArrayAdapter);
+        wordList.setTextFilterEnabled(true);
+        List<Word> words = wordsAdapter.getAllWords();
+
+        try {
+            Log.d(TAG, "Inserting words...");
+            for (Word word : words){
+                String wordAdd = word.getEnglish();
+                //  Log.d(TAG,wordAdd);
+                wordListItems.add(0, wordAdd);
+            }
+        }catch (NullPointerException npe){
+            Log.d(TAG,"Алдаа : "+npe);
+        }catch (Exception e){
+            Log.d(TAG,"Алдаа : "+e);
+        }
+
+        wordList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object o = parent.getItemAtPosition(position);
+                final String english = o.toString();
+                Log.d(TAG, "Дарагдсан лист дээрх үг : " + english);
+                editor.putString("SearchedWord", english);
+                editor.commit();
+                Fragment wordLookFragment = new WordLookFragment();
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fm.beginTransaction();
+                fragmentTransaction.replace(R.id.container, wordLookFragment);
+                fragmentTransaction.addToBackStack("Word view").commit();
+            }
+        });
+        myArrayAdapter.notifyDataSetChanged();
+
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+
+        SearchView.OnQueryTextListener textChangeListener = new SearchView.OnQueryTextListener()
+        {
+            @Override
+            public boolean onQueryTextChange(String newText)
+            {
+                myArrayAdapter.getFilter().filter(newText);
+                return true;
+            }
+            @Override
+            public boolean onQueryTextSubmit(String query)
+            {
+                myArrayAdapter.getFilter().filter(query);
+                return true;
+            }
+        };
+        searchView.setOnQueryTextListener(textChangeListener);
         return true;
     }
     @Override
