720b6ec0e54d2c8a42898821546f102a72228c3b
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-10-23 20:24:21 +0800

    үг нэмэх popup болгосон

--- app/src/main/java/com/memorize/Main.java
+++ app/src/main/java/com/memorize/Main.java
@@ -1,45 +1,66 @@
 package com.memorize;
+
+import android.app.ProgressDialog;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.memorize.R;
+import com.memorize.component.MyAlertDialog;
+import com.memorize.database.WordsAdapter;
+import com.memorize.model.Word;
+import com.memorize.service.ServerRequest;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import com.memorize.activity.Settings;
 import com.memorize.activity.Web;
-import com.memorize.activity.WordAdd;
 import com.memorize.activity.WordDetail;
 import com.memorize.activity.WordRemember;
 import com.memorize.database.RememberWordsAdapter;
-import com.memorize.database.WordsAdapter;
 import com.memorize.model.RememberWord;
-import com.memorize.model.Word;
 import com.memorize.sensor.ShakeEventManager;
 
 public class Main extends AppCompatActivity
@@ -49,17 +70,27 @@ public class Main extends AppCompatActivity
     boolean doubleBackToExitPressedOnce = false;
 
     public static final String PREFER_NAME = "Memorize";
-    private SharedPreferences sharedPreferences;
-    SharedPreferences.Editor editor;
-    ListView wordList;
-    WordsAdapter wordsAdapter;
-    RememberWordsAdapter rememberWordsAdapter;
-    private ShakeEventManager shakeEventManager;
-    NavigationView navigationView;
-    final ArrayList<String> wordListItems = new ArrayList<String>();
-    ArrayAdapter<String> myArrayAdapter;
 
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private ListView wordList;
+    private List<NameValuePair> params;
     final Context context = this;
+    private WordsAdapter wordsAdapter;
+    private RememberWordsAdapter rememberWordsAdapter;
+    private ShakeEventManager shakeEventManager;
+    private NavigationView navigationView;
+    final ArrayList<String> wordListItems = new ArrayList<String>();
+    private ArrayAdapter<String> myArrayAdapter;
+
+    private TextView englishWordInput;
+    private TextView wordTypeInput;
+    private TextView mongolianWordInput;
+    private Button saveButton;
+    private MyAlertDialog alert;
+    private Animation shake;
+    private Animation myshake;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -127,19 +158,146 @@ public class Main extends AppCompatActivity
 
     }
 
+    // Шинэ үг нэмэх dialog
     public void onCreateDialog() {
-
-        final Dialog dialog = new Dialog(this,R.style.AlertDialog);
+        final Dialog dialog = new Dialog(this);
         dialog.setContentView(R.layout.word_add);
-        dialog.setTitle("Title...");
+        dialog.setTitle("Шинэ үг нэмэх");
+        WindowManager.LayoutParams layoutParam = new WindowManager.LayoutParams();
+        layoutParam.copyFrom(dialog.getWindow().getAttributes());
+        layoutParam.width = WindowManager.LayoutParams.MATCH_PARENT;
+        layoutParam.height = WindowManager.LayoutParams.WRAP_CONTENT;
 
-        TextView wordMon = (TextView) dialog.findViewById(R.id.mongolianInput);
-        TextView wordtype = (TextView) dialog.findViewById(R.id.typeInput);
-        TextView wordEng = (TextView) dialog.findViewById(R.id.englishInput);
+        alert = new MyAlertDialog();
+        wordsAdapter = new WordsAdapter(this);
+        englishWordInput = (TextView)findViewById(R.id.englishWordInput);
+        wordTypeInput = (TextView)findViewById(R.id.wordTypeInput);
+        mongolianWordInput = (TextView)findViewById(R.id.mongolianWordInput);
 
+        shake = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.shake);
+        myshake = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.myshake);
+
+        saveButton = (Button) findViewById(R.id.newWordSave);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+//        saveButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                addNewWord();
+//            }
+//        });
         dialog.show();
+
+        dialog.getWindow().setAttributes(layoutParam);
+
+    }
+    public void addNewWord(){
+        if (!validate()){
+            Toast.makeText(getBaseContext(), "Үг нэмхэд алдаа гарлаа", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        String english = englishWordInput.getText().toString();
+        String type = wordTypeInput.getText().toString();
+        String mongolia = mongolianWordInput.getText().toString();
+
+        params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("eng", english));
+        params.add(new BasicNameValuePair("type", type));
+        params.add(new BasicNameValuePair("mon", mongolia));
+
+        ServerRequest sr = new ServerRequest();
+        JSONObject json = sr.getJSON("https://memorize-server-tortuvshin.c9users.io/addword",params);
+
+        if(json != null){
+            try{
+                String jsonstr = json.getString("response");
+
+                Toast.makeText(getApplication(),jsonstr,Toast.LENGTH_LONG).show();
+
+                Log.d("Hello", jsonstr);
+
+            }catch (JSONException e) {
+                e.printStackTrace();
+                Log.e("Алдаа"," алдаа"+e);
+            }
+        }
+        final ProgressDialog progressDialog = new ProgressDialog(Main.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Шинэ үг нэмж байна...");
+        progressDialog.show();
+
+        // TODO: Implement your own signup logic here.
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        alert.showAlertDialog(Main.this, "    Амжилттай бүртгэгдлээ.", "Шинэ үг амжилттай нэмлээ...", true);
+                        new android.os.Handler().postDelayed(
+                                new Runnable() {
+                                    public void run() {
+                                        finish();
+                                    }
+                                }, 1000);
+                        Log.d(TAG, "Бүртгэл амжилттай боллоо...");
+                        String english = englishWordInput.getText().toString();
+                        String type = wordTypeInput.getText().toString();
+                        String mongolia = mongolianWordInput.getText().toString();
+                        wordsAdapter.addWord(new Word(english, type, mongolia));
+
+
+                        Toast.makeText(getApplicationContext(), "Амжилттай нэмлээ", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, "Амжилттай нэмлээ");
+                        progressDialog.dismiss();
+                    }
+                }, 1000);
+
     }
 
+    public boolean validate(){
+        boolean valid = true;
+
+        String english = englishWordInput.getText().toString();
+        String type = wordTypeInput.getText().toString();
+        String mongolia = mongolianWordInput.getText().toString();
+
+        Cursor words = wordsAdapter.checkWord(english);
+
+        if (english.isEmpty() || type.isEmpty() || mongolia.isEmpty()) {
+            englishWordInput.startAnimation(shake);
+            wordTypeInput.startAnimation(shake);
+            mongolianWordInput.startAnimation(shake);
+            englishWordInput.setError("талбар хоосон байна");
+            wordTypeInput.setError("талбар хоосон байна");
+            mongolianWordInput.setError("талбар хоосон байна");
+            valid = false;
+        } else {
+            englishWordInput.setError(null);
+            wordTypeInput.setError(null);
+            mongolianWordInput.setError(null);
+        }
+
+        if (words == null) {
+            alert.showAlertDialog(getApplicationContext(), "Error", "Database query error", false);
+            Log.e(TAG, "Өгөгдлийн сангийн query алдаатай байна");
+            valid = false;
+        } else {
+            startManagingCursor(words);
+            if (words.getCount() > 0) {
+                alert.showAlertDialog(getApplicationContext(), "Алдаа", "Бүртгэлтэй үг байна", false);
+                Log.e(TAG, "Бүртгэлтэй үг байна");
+                stopManagingCursor(words);
+                words.close();
+                valid = false;
+            }
+        }
+
+        return valid;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
